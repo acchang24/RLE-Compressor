715b5dc5a2ed5ea773d9271e5cc248e97bba615b
Alex Chang <acchang24@gmail.com>
2025-02-26 16:38:20 -0800

    rle in main

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <vector>
-#include "RleCompressor/RleData.h"
 #include "RleCompressor/RleFile.h"
 
 int main(int argc, const char* argv[])
 {
-	RleData data;
-	data.Compress(std::vector<int8_t>{1, 2});
-
 	RleFile file;
-	file.CreateArchive("what");
+	file.CreateArchive("data/Rumba Dancing.dae");
+	file.CreateArchive("data/sponza.obj");
 
-	std::cout << "Compressor i guess\n";
 	return 0;
 }
